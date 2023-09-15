1a9244680c3883aa40216edf8362d0b05fcb9883
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:47:27 -0300

    criando variavel

---
+++ aula.js
@@ -0,0 +1,16 @@
+// Aqui é um alert
+alert("Olá Mundo");
+
+/*
+aprendendo JavaScript
+Top
+*/
+// imprimindo mensagem na tela
+document.write("Meu Nome é Lucca");
+
+// usando console
+console.log("Olá, Tudo Bem?");
+
+// criando nossa primeira varável
+let mensagem = "Olá, Mundo";
+console.log("Esse console é da variável:",mensagem)
