7bdf378bccf15c41b944389c6d8d413eff037e8b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:15:50 -0300

    Transformando variaveis em textos e numeros

---
+++ texto-numeros.js
@@ -0,0 +1,14 @@
+let numFloat = 123.5422;
+// para converter a variavel para string : "variavel".toString(); ou String(variavel);
+let numString = numFloat.toString();
+
+let nomeAluno1 = "Pedro Henrique";
+
+let txt = "Estão chegando as provas";
+
+// variavel.slice(0,5) pega o termos que estão na posição de 0 á 5
+// variavel.indexOf("as") retorna o valor da primeira ocorrência, variavel.lastIndexOf("as") retorna o valor da últim ocorrência
+// variavel.length serve para contar o numero de caracteres de uma string por exemplo
+// Para converter strings para numbers usa-se parseFloat ou Number seguido da variavel entre ()
+// varivel.substr(6,8) retorna os 8 elementos após a 6 posição 
+console.log(txt.substr(6,8));
