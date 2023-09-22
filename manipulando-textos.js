ae06904dfb1c0db69f98ca62f85ed7fdaa521e21
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:34:42 -0300

    Manipulando Operadores

---
+++ manipulando-textos.js
@@ -0,0 +1,16 @@
+let txt = "Estão chegando as provas";
+
+let num = 123.5432;
+
+
+// variavel.replace("povas", "avaliações") subistitúi um termo dentro da variável texto
+//console.log(txt.replace("provas", "avaliações"));
+
+// variavel.toUpperCase() coloca a variavel em letra maiúscula
+// variavel.LowerCase() coloca a variavel em letra minúscula
+//console.log(txt.toUpperCase());
+
+//variavel.toFixed(2) só pega os valores até a cegunda casa após a virgula
+ console.log(num.toFixed(2))
+
+
---
+++ operadores.js
@@ -0,0 +1,14 @@
+// declaração de variavel
+let num1 = 10;
+let num2 = 5;
+
+// Operções Matemáticas com as variáveis
+// + (soma), - (subtração), * (multiplicação), / (divisão)
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+// Exibindo resultados no console
+// no console.log para concatenar pode se usar + ou ,
+console.log("O resultado da divisão é: " + divisao);
