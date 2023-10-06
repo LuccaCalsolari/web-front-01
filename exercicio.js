63c0beb48a412129b2f91abaaeac01932ea558af
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:27:45 -0300

    Adicionando condicionais

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+let frutas = ['morango','banana','mamão','pera'];
+
+if (frutas.includes('abacaxi')) {
+    console.log('abacaxi existe na lista');
+} else if (frutas.includes('pera')) {
+    console.log('Pera existe na lista');
+} else {
+    console.log('Nem pera nem abacaxi existem na lista');
+}
