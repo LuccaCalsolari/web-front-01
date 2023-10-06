957736050662a7e10ba81730ab98df918083a294
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:42:16 -0300

    switch case

---
+++ switch.js
@@ -0,0 +1,12 @@
+let teste = 3;
+
+switch (teste) {
+    case 1:
+        console.log('foi de primeira');
+        break;
+    case 2:
+        console.log('foi na segunda');
+        break;
+    default:
+        console.log('Nem na primeira nem na segunda');
+}
