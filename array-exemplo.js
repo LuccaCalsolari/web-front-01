7266e7bc29513ea5df899a36589bbe77aebacf58
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:51:02 -0300

    exercicios aula-06

---
+++ array-exemplo.js
@@ -0,0 +1,5 @@
+const times = ['Flamengo', 'Paulista', 'Botafogo'];
+
+times.push("Real Madrid");
+
+console.table(times);
---
+++ exericio1.js
@@ -0,0 +1,9 @@
+let i = 0;
+
+while (i <= 5){
+    console.log(i);
+    i++;
+}
+
+
+
